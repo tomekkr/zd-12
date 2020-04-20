42e31847c31fb7b48f43e4b7e26df5e6245ea9d0
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-04-20 21:56:38 +0200

    changed names: foodTax > FOOD_TAX, drinkTax > DRINK_TAX

--- src/PriceService.java
+++ src/PriceService.java
@@ -1,15 +1,15 @@
 class PriceService {
-    private final double foodTax = 0.15;
-    private final double drinkTax = 0.1;
+    private final static double FOOD_TAX = 0.15;
+    private final static double DRINK_TAX = 0.1;
 
     public double calculateGrossPrice(Product product) {
         double grossPrice;
         switch (product.getCategory()) {
             case "jedzenie":
-                grossPrice = product.getNetPrice() + product.getNetPrice() * foodTax;
+                grossPrice = product.getNetPrice() + product.getNetPrice() * FOOD_TAX;
                 break;
             case "napoje":
-                grossPrice = product.getNetPrice() + product.getNetPrice() * drinkTax;
+                grossPrice = product.getNetPrice() + product.getNetPrice() * DRINK_TAX;
                 break;
             default:
                 System.err.println("Nieznana kategoria, nie można obliczyć kwoty brutto.");
